3437e0171ca8f938ca7660ccd19fa18c64ca9993
PussyAss <Deadspace20111@yandex.ru>
2018-05-18 15:04:56 +0700

    Создание классов car & minivan

---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+            Car myCar = new Car(80);
+
+            myCar.Speed = 50;
+            Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+            Console.ReadLine();
+
+            //Создать объект MiniVan
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 10;
+            Console.WriteLine("My van is going {0} MPH", myVan.Speed);
+            Console.ReadLine();
+        }
+    }
+}
